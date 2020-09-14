f2a7668d0fdf01e98461efb9aee5e2685db8dbbd
suozhiwen <suozhiwen@163.com>
2020-09-14 13:50:11 +0800

    主页评分行

--- src/components/test/index.vue
+++ src/components/test/index.vue
@@ -4,6 +4,20 @@
       <div class="flex-item">flex item 1</div>
       <div class="flex-item">flex item 2</div>
     </div>
+    <div class="flex-container">
+      <div style="display: flex;width: 70%">
+        <img style="width:50px;height:50px" src="../../assets/img/logo.png" />
+        <div>
+          <sapn style="display: flex">战火与秩序</sapn>
+          <span>人、兽、魔3大种族，4大兵种，12种排列，这才叫策略手游！</span>
+        </div>
+      </div>
+
+      <div>
+        <div>1196人评分</div>
+        <div>6.2</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
