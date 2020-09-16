f03196dda9692aa1a79489874de838ecad85f568
suozhiwen <suozhiwen@163.com>
2020-09-16 17:46:58 +0800

    重写布局

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,39 +1,11 @@
 <template>
-  <div class="div_top" style="padding: 45px 100px 55px;width: 1070px">
-    <div>
-      <img
-        src="https://img.tapimg.com/market/images/09b681d7e5d888364c85bdc67a69ba1c.jpg?imageMogr2/auto-orient/thumbnail/2080x/strip/gravity/Center/crop/2080x828/format/jpg/quality/80/interlace/1"
-      />
-    </div>
-    <div style="margin-top: 20px;">
-      <el-row :gutter="100">
-        <el-col :span="16" style="border:1px solid">
-          <div style="background:#FFFFFF">
-            <div class="title">
-              <span style="float: left;
-    margin: 0 19px 0px">来自广告</span>
-              <div>12312</div>
-            </div>
+  <div class="div_top">
+    <div style="flex:1;border: 1px solid">0</div>
+    <div style="border: 1px solid;width:900px;height: 1000px">
 
-            <div>
-              <img
-                style="width:100%;height:300px"
-                src="https://static-tapad.tapdb.net/MTY5MjUzNDFANWNiZWFiNTE3ZDI1NA.jpg?imageView2/0/w/1080/q/80/format/jpg/interlace/1/ignore-error/1"
-              />
-            </div>
-            <div>
-              <img
-                style="width:50px;height:50px"
-                src="https://img.tapimg.com/market/lcs/efc4373e78ce7a2a0973bf3da7317884_360.png?imageView2/1/w/240/q/80/interlace/1/ignore-error/1"
-              />
-            </div>
-          </div>
-        </el-col>
-        <el-col :span="8" style="border:1px solid">
-          <div>222</div>
-        </el-col>
-      </el-row>
+
     </div>
+    <div style="flex:1;border: 1px solid">2</div>
   </div>
 </template>
 <script>
@@ -41,12 +13,16 @@ export default {}
 </script>
 <style scoped lang="less">
 .div_top {
+  display: flex;
   background-color: #f2f2f2;
+  height: 100%;
+  // flex-wrap: wrap;
   img {
     width: 100%;
     height: 500px;
   }
 }
+
 .title {
   height: 50px;
   line-height: 50px;
